8bd164b0fdcd8d5d6c1263db85d9528602badfd7
Renata Martinez <renata.martinez@mayor.cl>
2021-11-05 12:25:03 -0300

    Update Automata_2p.py

--- Automata/Automata_2p.py
+++ Automata/Automata_2p.py
@@ -1,79 +1,114 @@
-#%%
 import numpy as np
+
+################estas son las librerias para hacer graficas
+
 import matplotlib.pyplot as plt
+
 import matplotlib as mpl
 
+##############################
+### tamaño de la matrix
 filas = columnas = 4
+### Tiempo de iteracion o cantidad de matrices  
+time = 6
 
 #Generacion de matriz aleatoria. >=0 <2
+
 mat_X = np.random.randint(0,2,size=(filas, columnas))
 
+
 #Cambio ceros a "-1"
+
 for f in range(filas):
+
     for c in range(columnas):
+
+  # print(f,c, matrix[f,c])
+
+     
+
       if mat_X[f,c] == 0:
+
         mat_X[f,c] = -1
 
-mat_Y = mat_X
-mat_Z = mat_X
 
-#%%Repeticiones
-times = 6
+#truco, alternative a for
 
-#%%Definicion de colores
+# matriz = np.matrix(np.where(matriz>0,matriz,-1))
+
+mat_Y = np.copy(mat_X)
+
+mat_Z = np.copy(mat_X)
+
+
+##### AQUI SE DEFINE SOLO DOS COLORES PARA QUE -1 Y 1 TOME ROJO O NEGRO
+
 cmap = mpl.colors.ListedColormap(['r', 'k'])
+
 bounds = [0., 0.5, 1.]
+
 norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
 
-matrices = []
+## ESTA ULTIMA ES PARA DEFINIR SOLO LOS BORDES.
 
-#%%Bordes, aplicación de la Regla
-for iT in range(times):
+#############################
+
+for it in range(time):
+
     for f in range(filas):
+
         f_mas = f+1
+
         f_menos = f-1
+
         sum_mat_X = 0
-    
+
         for c in range(columnas):
+
             c_mas = c+1
+
             c_menos = c-1
 
+           
             if f_mas == filas:
+
                 f_mas = 0
-                
+             
             if f_menos == -1:
+
                 f_menos = filas-1
-                
+
             if c_mas == columnas:
+
                 c_mas = 0
 
             if c_menos == -1:
+
                 c_menos = columnas-1
 
+            # print(f_mas,f_menos,c_mas,c_menos)
+
             sum_mat_X = mat_X[f_mas,c]+mat_X[f_menos,c]+mat_X[f,c_mas]+mat_X[f,c_menos]
+
+            #print(sum_mat_X)
+           
             if sum_mat_X == 0:
+
                 mat_Z[f,c] = -1 * mat_Y[f,c]
-                print(mat_Z)
-    print("==============A================")
-    print(mat_Y)
-    print("==")
-    print(mat_X)
-    print("==")
-    print(mat_Z)
-    
-    mat_Y = mat_X
-    mat_X = mat_Z
-    print("==============B================")
-    print(mat_Y)
-    print("==")
-    print(mat_X)
-    print("==")
-    print(mat_Z)
-    #%%MOSTRAR LA EVOLUCION DE LA MATRIX mat_Y.
-    plt.imshow(mat_Y, interpolation='none',cmap=cmap, norm=norm)
 
-    #Se grafican las Matrices
-    plt.show()
+    #print("mat_X")
+    #print(mat_X);
+    #print("mat_Y")
+    #print(mat_Y);
+    mat_Y=np.copy(mat_X)                        
+    mat_X=np.copy(mat_Z)
+    nameMatrix='matX_'+str(it)+'.txt'
+    np.savetxt(nameMatrix, mat_X)
 
-for matriz in matrices:
-    print(matriz)
+
+
+
+
+
+
+#################################################
